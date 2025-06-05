e79fd964f295855979f11eefa4570a71c868ab55
ComplYue <compl.yue@icloud.com>
2025-06-05 19:15:05 +0800

    use vscode.lm tools

--- src/core/task/Task.ts
+++ src/core/task/Task.ts
@@ -1816,6 +1816,7 @@ export class Task extends EventEmitter<TaskEvents> implements TaskLike {
 	}
 
 	private async getSystemPrompt(): Promise<string> {
+		const vsclmtService= this.providerRef.deref()?.getVSCLMToolService()
 		const { mcpEnabled } = (await this.providerRef.deref()?.getState()) ?? {}
 		let mcpHub: McpHub | undefined
 		if (mcpEnabled ?? true) {
@@ -1868,6 +1869,7 @@ export class Task extends EventEmitter<TaskEvents> implements TaskLike {
 				provider.context,
 				this.cwd,
 				(this.api.getModel().info.supportsComputerUse ?? false) && (browserToolEnabled ?? true),
+				vsclmtService,
 				mcpHub,
 				this.diffStrategy,
 				browserViewportSize,
--- src/core/webview/ClineProvider.ts
+++ src/core/webview/ClineProvider.ts
@@ -53,6 +53,7 @@ import { Terminal } from "../../integrations/terminal/Terminal"
 import { downloadTask } from "../../integrations/misc/export-markdown"
 import { getTheme } from "../../integrations/theme/getTheme"
 import WorkspaceTracker from "../../integrations/workspace/WorkspaceTracker"
+import { VSCLMToolsService } from "../../services/vsclm/VSCLMToolsService"
 
 import { McpHub } from "../../services/mcp/McpHub"
 import { McpServerManager } from "../../services/mcp/McpServerManager"
@@ -109,6 +110,7 @@ export class ClineProvider
 	public get workspaceTracker(): WorkspaceTracker | undefined {
 		return this._workspaceTracker
 	}
+	private vsclmtService: VSCLMToolsService
 	protected mcpHub?: McpHub // Change from private to protected
 	private marketplaceManager: MarketplaceManager
 	private mdmService?: MdmService
@@ -148,6 +150,8 @@ export class ClineProvider
 		this.customModesManager = new CustomModesManager(this.context, async () => {
 			await this.postStateToWebview()
 		})
+
+		this.vsclmtService = new VSCLMToolsService(context)
 
 		// Initialize MCP Hub through the singleton manager
 		McpServerManager.getInstance(this.context, this)
@@ -311,6 +315,9 @@ export class ClineProvider
 		return this.clineStack[this.clineStack.length - 1]
 	}
 
+	public getVSCLMToolService(): VSCLMToolsService {
+		return this.vsclmtService
+	}
 	// returns the current clineStack length (how many cline objects are in the stack)
 	getClineStackSize(): number {
 		return this.clineStack.length
